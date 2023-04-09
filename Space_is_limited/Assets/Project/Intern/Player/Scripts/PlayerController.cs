e5621b8b240ab6d567536a03af060cdecd9b305d
Konrad Ebel <konrad.ebel.28@gmail.com>
2023-04-09 10:24:39 +0200

    Changing input system and movement of player

--- Space_is_limited/Assets/Project/Intern/Player/Scripts/PlayerController.cs
+++ Space_is_limited/Assets/Project/Intern/Player/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
 
 /// <summary>
 /// controles the Movement of the player
@@ -12,13 +13,18 @@ public class PlayerController : Singleton<PlayerController>
     #endregion
 
     #region variables
-    [Tooltip("Toggles the speed of the player")]
-    [SerializeField] float speed = 0f;
-    [Tooltip("Toggles the maximum of the Jump Speed")]
-    [SerializeField] float maxJumpSpeed = 0f;
+    [Tooltip("Jumpforce")]
+    [SerializeField] float jumpForce = 20f;
+    [Tooltip("Toggles the force to move")]
+    [SerializeField] float sideForce = 10f;
+    [Tooltip("Toggles the maximum speed the player can reach")]
+    [SerializeField] float maxSideSpeed = 7f;
     [Tooltip("Toggles the look direction of the player")]
     [SerializeField] bool flippedLeft = true;
     [SerializeField] GameObject Appearance;
+
+    private bool jump;
+    private float moveSidewardsInput;
 
     #endregion
 
@@ -34,52 +40,81 @@ public class PlayerController : Singleton<PlayerController>
     {
         Controller = GetComponent<Rigidbody2D>();
     }
+    #endregion
+
+    #region Input Functions
+    /// <summary>
+    /// Jump or fall faster
+    /// </summary>
+    /// <param name="callback"></param>
+    public void JumpOrFall(CallbackContext callback)
+    {
+        jump = callback.ReadValue<float>() > 0.5f;
+
+        if (jump && GroundChecker.Instance.onGround)
+        {
+            Controller.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
 
     /// <summary>
-    /// movement-update
+    /// Moves the player sideways
     /// </summary>
-    private void FixedUpdate()
+    /// <param name="callback"></param>
+    public void MoveSidewards(CallbackContext callback)
     {
-
-        float inputVector = Input.GetAxis("Horizontal");
-        bool isOnGround = GroundChecker.Instance.onGround;
-
-        float yVelocity;
-
-        if (Input.GetKey(KeyCode.W) && isOnGround)
-        {
-            yVelocity = maxJumpSpeed;
-        }
-        else if (!Input.GetKey(KeyCode.W) && Controller.velocity.y > 0)
-        {
-            yVelocity = Controller.velocity.y * 0.8f;
-        }
-        else
-        {
-            yVelocity = Controller.velocity.y;
-        }
-
-        Controller.velocity = new Vector2(speed * inputVector, yVelocity);
+        moveSidewardsInput = callback.ReadValue<float>();
 
         //Change the look of the player (inclusive colliders)
-        if (inputVector > 0 && flippedLeft)
+        if (moveSidewardsInput > 0 && flippedLeft)
         {
             flip();
         }
-        else if (inputVector < 0 && !flippedLeft)
+        else if (moveSidewardsInput < 0 && !flippedLeft)
         {
             flip();
         }
+    }
 
-        //when the s-key is pressed lets the player fall down through platforms
-        if (Input.GetKey(KeyCode.S))
+    /// <summary>
+    /// Lets the player fall through platforms
+    /// </summary>
+    /// <param name="callback"></param>
+    public void FallTrough(CallbackContext callback)
+    {
+        if (callback.performed)
         {
             Appearance.layer = LayerMask.NameToLayer("PlayerOffPlatform");
-        }
-        else
+        } else
         {
             Appearance.layer = LayerMask.NameToLayer("Player");
         }
+    }
+
+    private void FixedUpdate()
+    {
+        // Move sidewards
+        AccelerateTo(moveSidewardsInput * sideForce, Controller.velocity.x, Vector2.right);
+
+        // Fall fast
+        if (!jump && Controller.velocity.y > 0)
+        {
+            Controller.velocity = new Vector2(Controller.velocity.x, Controller.velocity.y * 0.8f);
+        }
+    }
+
+    /// <summary>
+    /// Accelerates to a specific speed
+    /// </summary>
+    /// <param name="targetVelocity"></param>
+    /// <param name="currentVelocity"></param>
+    private void AccelerateTo(float targetVelocity, float currentVelocity, Vector3 direction)
+    {
+        var deltaV = targetVelocity - currentVelocity;
+        var accel = deltaV / Time.deltaTime;
+
+        accel = Mathf.Clamp(accel, -maxSideSpeed, maxSideSpeed);
+        Controller.AddForce(direction * accel, ForceMode2D.Force);
     }
     #endregion
 
